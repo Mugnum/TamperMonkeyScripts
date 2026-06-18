bdfb3ca5538b05b0bb62b1b596144850d7cd2ece
Sergey Syrtsev <syrtsevser@gmail.com>
2026-06-18 06:52:40 +0300

    Removed debug text.

--- Scripts/chatgpt-remove-black-background.user.js
+++ Scripts/chatgpt-remove-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT: Remove black background
 // @namespace    Mugnum.Scripts.ChatGPT
-// @version      1.1.0
+// @version      1.2.0
 // @description  Replaces pure black background on OLED screens on ChatGPT
 // @author       Mugnum
 // @match        https://chatgpt.com/*
@@ -11,71 +11,30 @@
 // ==/UserScript==
 
 (() => {
-    //const replacement = '#212121';
-    const commonStyle = 'var(--bg-primary)';
-    const codeStyle = '#202020';
+	GM_addStyle(`
+		html.dark {
+			--main-surface-primary: #272727 !important;
+			--main-surface-secondary: #272727 !important;
+		}
 
-    GM_addStyle(`
-    /*
-    @layer base {
-      html.dark,
-      html.dark :not(:where(.light, .light *)) {
-        --main-surface-primary: ${commonStyle} !important;
-      }
-    }
+		body,main,[class*="bg-token-main-surface-primary"] {
+			background-color: #272727 !important;
+		}
 
-    html.dark {
-      --main-surface-primary: ${commonStyle} !important;
-    }
+		.bg-token-sidebar-surface-primary {
+			background-color: #202020;
+		}
 
-    @layer utilities {
-        .bg-token-bg-elevated-secondary {
-            background-color: ${codeStyle} !important;
-        }
-    }
+		.bg-\\(--sidebar-surface-primary\\) {
+			background-color: #202020;
+		}
 
-    @layer utilities {
-        .bg-token-sidebar-surface-primary {
-            background-color: ${commonStyle} !important;
-        }
-    }
+		html.dark, html.dark :not(:where(.light,.light *)) {
+			--main-surface-primary: #272727ed;
+		}
 
-    @layer utilities {
-        .bg-\\(--sidebar-surface-primary\\) {
-            background-color: ${commonStyle} !important;
-        }
-    }
-
-    @layer utilities {
-        .dark\:bg-token-bg-secondary-surface:where(.dark, .dark *):not(:where(.dark .light, .dark .light *)) {
-            background-color: ${commonStyle} !important;
-        }
-    }
-    */
-
-	html.dark {
-		--main-surface-primary: #272727 !important;
-		--main-surface-secondary: #272727 !important;
-	}
-
-	body,main,[class*="bg-token-main-surface-primary"] {
-		background-color: #272727 !important;
-	}
-
-	.bg-token-sidebar-surface-primary {
-		background-color: #202020;
-	}
-
-	.bg-\\(--sidebar-surface-primary\\) {
-		background-color: #202020;
-	}
-
-	html.dark, html.dark :not(:where(.light,.light *)) {
-		--main-surface-primary: #272727ed;
-	}
-
-	.bg-token-bg-elevated-secondary {
-		background-color: #202020;
-	}
-  `);
+		.bg-token-bg-elevated-secondary {
+			background-color: #202020;
+		}
+	`);
 })();
